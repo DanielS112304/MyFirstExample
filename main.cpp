39e70e14f038c5435f2b3ff50ab945b7981b2ccb
Daniel S <dsoto16@csuchico.edu>
2025-09-17 11:49:05 -0700

    Adding build yml file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
-<<<<<<< HEAD
 {   
     cout << "Yo its me, lil d" << endl;
     cout << "Hi, please enter two whole numbers: ";
@@ -17,24 +18,5 @@ int main()
     cout << "Square Root of " << x << " is " << sqrt(x) << endl;
     cout << "Square Root of " << y << " is " << sqrt(y) << endl;
     cout << x << "^" << y << "=" << pow(x, y) << endl;
-    
-=======
-{
-    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    std::cout << "Hi, please enter two whole numbers: ";
-
-    int x,y;
-
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
-
->>>>>>> 4beaca6a6ec6f87415e1befc3e887fbe0112bb3a
     return 0;
-
-}
+};
